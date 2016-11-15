87106b0dd15774881c11d96620a9b925d7b5a357
Patrick Smith <patrick@burntcaramel.com>
2016-11-15 16:24:12 +1100

    Use celsius in weather

--- app/models/concerns/weather.rb
+++ app/models/concerns/weather.rb
@@ -1,39 +1,16 @@
 module Weather
   extend ActiveSupport::Concern
 
-  def weather_for_address(address)
-    uri = URI('https://query.yahooapis.com/v1/public/yql')
-    uri.query = URI.encode_www_form(
-      q: "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='#{ address }')",
-      format: 'json'
-    )
-
-    # Create client
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-
-    # Create Request
-    req =  Net::HTTP::Get.new(uri)
-
-    # Fetch Request
-    res = http.request(req)
-    # Parse JSON into hashes and arrays
-    json = JSON.parse(res.body) 
-    json.dig('query', 'results', 'channel')
-  rescue StandardError => e
-    puts "HTTP Request failed (#{e.message})"
-  end
-
   class_methods do
     def weathers_for_addresses(addresses)
       uri = URI('https://query.yahooapis.com/v1/public/yql')
 
-      addresses_commas = addresses.map{ |address| "'#{ address }'" }.join(',') 
+      addresses_commas = addresses.map{ |address| "'#{ address }'" }.join(',')
 
       uri.query = URI.encode_www_form(
-        q: "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text in (#{ addresses_commas }))",
-        format: 'json'
+        q: "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text in (#{ addresses_commas })) and u='c'",
+        format: 'json',
+        u: 'c'
       )
 
       # Create client
