7aca2474cf9a539f8e371fe09114cc5263706ca2
Patrick Smith <pgwsmith@gmail.com>
2016-11-24 22:06:48 +1100

    Fix foreign key in messages migration

--- db/migrate/20161123013438_create_messages.rb
+++ db/migrate/20161123013438_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.references :guest, foreign_key: true
+      t.references :guest, foreign_key: { to_table: :users }
       t.references :listing, foreign_key: true
       t.text :content
 
